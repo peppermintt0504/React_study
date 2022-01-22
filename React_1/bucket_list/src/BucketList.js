2dc144591cde052f384ff194b7e3ef429b04bb9f
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-01-22 15:35:02 +0900

    Week3

--- React_1/bucket_list/src/BucketList.js
+++ React_1/bucket_list/src/BucketList.js
@@ -1,27 +1,36 @@
 // 리액트 패키지를 불러옵니다.
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const BucketList = ({ list }) => {
-
-    const my_lists = list;
+const BucketList = (props) => {
+    console.log(props);
+    const my_lists = props.list;
 
     return (
-        <div>
-            {
-                my_lists.map((list, index) => {
-                    console.log(list);
-                    return (<List_item key={index}>{list}</List_item>);
-                })
-            }
-        </div>
+        <ListStyle>
+            {my_lists.map((list, index) => {
+                return (
+                    <ItemStyle className="list_item" key={index} onClick={() => { }}>
+                        {list}
+                    </ItemStyle>
+                );
+            })}
+        </ListStyle>
     );
-}
-const List_item = styled.div`
-    padding: 16px;
-    margin: 8px;
-    background-color: aliceblue;
-}   
+};
+
+const ListStyle = styled.div`
+display: flex;
+flex-direction: column;
+height: 100%;
+overflow-x: hidden;
+overflow-y: auto;
+`;
+
+const ItemStyle = styled.div`
+padding: 16px;
+margin: 8px;
+background-color: aliceblue;
 `;
 
 export default BucketList;

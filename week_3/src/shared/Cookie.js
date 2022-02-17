7dad5876d44162125249a1d84eb2cb16d2230f97
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-02-18 01:54:13 +0900

    Update Cookie.js

--- week_3/src/shared/Cookie.js
+++ week_3/src/shared/Cookie.js
@@ -11,7 +11,7 @@ const getCookie = (name) => {
     const setCookie = (name, value, exp = 5) => {
     let date = new Date();
     date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
-  
+
     document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
 };
 
